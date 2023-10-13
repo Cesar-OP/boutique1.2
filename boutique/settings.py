fb4999a1b8c26892d49bc283435200d1626f95a9
Cesar <adm.anasantosjesus@gmail.com>
2023-10-13 11:18:04 -0300

    correcting the path

--- boutique/settings.py
+++ boutique/settings.py
@@ -3,7 +3,7 @@ import os
 from decouple import config, Csv
 import django_heroku
 import dj_database_url
-from cart import context_processors
+from cart.context_processors import cart
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
